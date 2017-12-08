79ecec46d435f3756498ecbf5d7b3af0cc61ffd2
AnastasiyaTatarenko <thisishorosho44@gmail.com>
2017-12-08 13:20:48 +0300

    tests DAL

---
+++ Films/test/films/DAL/FilmsDalTest.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package films.DAL;
+
+import films.model.Films;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Татаренко
+ */
+public class FilmsDalTest {
+    
+    public FilmsDalTest() {
+    }
+    
+        @Test
+    public void testSelectAll() {
+        System.out.println("selectAll");
+        FilmsDal instance = new FilmsDal();
+        List<Films> result = instance.selectAll();
+        assertTrue(result!=null && result.size()>0);
+    }
+
+    /**
+     * Test of selectById method, of class FilmsDal.
+     */
+    @Test
+    public void testSelectById() {
+        System.out.println("selectById");
+        int id = 1;
+        FilmsDal instance = new FilmsDal();
+        Films result = instance.selectById(id);
+        assertTrue(result!=null && result.getId()==id);
+    }
+
+    /**
+     * Test of update method, of class FilmsDal.
+     */
+    @Test
+    public void testUpdate() {
+       assertTrue(true);
+    }
+
+    /**
+     * Test of insert method, of class FilmsDal.
+     */
+    @Test
+    public void testInsert() {
+         assertTrue(true);
+    }
+
+    /**
+     * Test of delete method, of class FilmsDal.
+     */
+    @Test
+    public void testDelete() {
+        assertTrue(true);
+    }
+       
+}
