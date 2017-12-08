882f85ed27620f80b53e5d463eacfe26c74cf579
AnastasiyaTatarenko <thisishorosho44@gmail.com>
2017-12-08 13:56:38 +0300

    Tests

--- Films/test/films/model/UserTest.java
+++ Films/test/films/model/UserTest.java
@@ -40,7 +40,7 @@ public class UserTest {
     /**
      * Test of getId method, of class User.
      */
-    @Test
+    /*@Test
     public void testGetId() {
         System.out.println("getId");
         User instance = new User();
@@ -54,7 +54,7 @@ public class UserTest {
     /**
      * Test of getLogin method, of class User.
      */
-    @Test
+    /*@Test
     public void testGetLogin() {
         System.out.println("getLogin");
         User instance = new User();
@@ -68,7 +68,7 @@ public class UserTest {
     /**
      * Test of getPassword method, of class User.
      */
-    @Test
+    /*@Test
     public void testGetPassword() {
         System.out.println("getPassword");
         User instance = new User();
@@ -82,7 +82,7 @@ public class UserTest {
     /**
      * Test of setId method, of class User.
      */
-    @Test
+    /*@Test
     public void testSetId() {
         System.out.println("setId");
         int id = 0;
@@ -95,7 +95,7 @@ public class UserTest {
     /**
      * Test of setLogin method, of class User.
      */
-    @Test
+    /*@Test
     public void testSetLogin() {
         System.out.println("setLogin");
         String login = "";
@@ -108,7 +108,7 @@ public class UserTest {
     /**
      * Test of setPassword method, of class User.
      */
-    @Test
+    /*@Test
     public void testSetPassword() {
         System.out.println("setPassword");
         String password = "";
@@ -122,14 +122,23 @@ public class UserTest {
      * Test of hashCode method, of class User.
      */
     @Test
-    public void testHashCode() {
-        System.out.println("hashCode");
-        User instance = new User();
-        int expResult = 0;
-        int result = instance.hashCode();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testCreateUser() {
+        System.out.println("test User createUser");
+        
+        User user = new User(1, "login", "password");
+        assertEquals(user.getId(), 1);
+        assertEquals(user.getLogin(), "login");
+        assertEquals(user.getPassword(), "password");
+        
+        
+        user.setId(2);
+        assertEquals(user.getId(), 2);
+        
+        user.setLogin("Alex");
+        assertEquals(user.getLogin(), "Alex");
+        
+        user.setPassword("123");
+        assertEquals(user.getPassword(), "123");
     }
 
     /**
@@ -137,20 +146,20 @@ public class UserTest {
      */
     @Test
     public void testEquals() {
-        System.out.println("equals");
-        Object obj = null;
-        User instance = new User();
-        boolean expResult = false;
-        boolean result = instance.equals(obj);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("test User equals");
+       
+        User user1 = new User(1, "login", "password");        
+        User user2 = new User(1, "login", "password");
+        User user3 = new User(2, "Alex", "123");
+        
+        assertTrue( user1.equals(user2)   );
+        assertFalse(user1.equals(user3)   );
     }
 
     /**
      * Test of toString method, of class User.
      */
-    @Test
+    /*@Test
     public void testToString() {
         System.out.println("toString");
         User instance = new User();
@@ -158,7 +167,5 @@ public class UserTest {
         String result = instance.toString();
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        fail("The test case is a prototype.");*/
     }
-    
-}
